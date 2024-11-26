542ad0c8383738b064b41eb5ebaf3cec75787971
darwin <darwinkoirala123@gmail.com>
2024-11-26 19:50:54 +0545

    just made some minor changes to lexer structure to make it memory safe

--- lexer/lexer.c
+++ lexer/lexer.c
@@ -3,10 +3,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+char *global_texts;
+int texts_count = 0;
+int text_size;
+
+char *peak(int i);
+char *consume(int i);
+
 Token *tokenize(char *texts) {
+  global_texts = texts;
   printf("Tokenizing...\n");
   int size_of_tokens = 1;
   int size_of_text = strlen(texts);
+  text_size = size_of_text;
   int token_count = 0;
 
   Token *tokens = malloc(sizeof(Token) * size_of_tokens);
@@ -14,24 +23,46 @@ Token *tokenize(char *texts) {
   tokens[0].value = "batman";
   tokens[0].tokenType = STRING;
 
-  for (int i = 0; i < size_of_text; i++) {
+  while (peak(0) != NULL) {
     token_count += 1;
 
+    // reallcating memory to array because C is ancient and doesn`t have a
+    // wapper for mutable array
     if (size_of_tokens <= token_count) {
       size_of_tokens *= 2;
       Token *temp = realloc(tokens, sizeof(Token) * size_of_tokens);
 
-      if(temp == NULL){
+      if (temp == NULL) {
         free(tokens);
         perror("Assigining Memory to Heap failed");
         exit(1);
       }
-
       tokens = temp;
     }
-
-    printf("%c", texts[i]);
+    printf("%c", *consume(0));
   }
-
+  // I am lazy to setup a debugger so here is the solution just print it simple
+  //printf("text count is %d", texts_count);
+  //printf("text size is %d", text_size);
   return tokens;
+}
+
+char *peak(int i) {
+  char *text;
+  if (texts_count + 1 > text_size) {
+    printf("reached here");
+    return NULL;
+  }
+  return &global_texts[texts_count + i];
+}
+
+char *consume(int i) {
+  char *text;
+  if (texts_count + 1 > text_size) {
+    printf("reached here");
+    return NULL;
+  }
+  char *temp = &global_texts[texts_count + i];
+  texts_count++;
+  return temp;
 }
--- main/main.c
+++ main/main.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[]) {
   char *text_in_file = malloc(buffer_size);
 
   readFile(argv[1], &text_in_file, &buffer_size);
-  //Need this for debugging . I am too lazy to setup a proper Debugger
+  //Need this for debugging . I am too lazy to setup a proper Debugger beacause I am batman
   //printf("%s", text_in_file);
 
   Token* tokens = tokenize(text_in_file);
