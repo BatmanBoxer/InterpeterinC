e3f086f1ea0450c4fe66556a3c27917df7befa85
darwin <darwinkoirala123@gmail.com>
2024-11-27 00:29:53 +0545

    i hate this language hope c goes to hell and rots in there

--- lexer/lexer.c
+++ lexer/lexer.c
@@ -1,4 +1,5 @@
 #include "tokens.h"
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,7 +9,7 @@ int texts_count = 0;
 int text_size;
 
 char *peak(int i);
-char *consume(int i);
+char *consume();
 
 Token *tokenize(char *texts) {
   global_texts = texts;
@@ -23,46 +24,54 @@ Token *tokenize(char *texts) {
   tokens[0].value = "batman";
   tokens[0].tokenType = STRING;
 
-  while (peak(0) != NULL) {
-    token_count += 1;
+  char buff[256] = {0}; // Initialize buffer to zero
+  int buff_counter = 0;
 
-    // reallcating memory to array because C is ancient and doesn`t have a
-    // wapper for mutable array
+  while (peak(0) != NULL) {
+    // Reallocating tokens array if necessary
     if (size_of_tokens <= token_count) {
       size_of_tokens *= 2;
       Token *temp = realloc(tokens, sizeof(Token) * size_of_tokens);
-
       if (temp == NULL) {
         free(tokens);
-        perror("Assigining Memory to Heap failed");
+        perror("Memory allocation failed during reallocation");
         exit(1);
       }
       tokens = temp;
     }
-    printf("%c", *consume(0));
+
+    if (isalpha(*peak(0))) {
+      char ch = *consume();
+      buff[buff_counter] = ch;
+      buff_counter++;
+    } else {
+      consume();
+    }
   }
-  // I am lazy to setup a debugger so here is the solution just print it simple
-  //printf("text count is %d", texts_count);
-  //printf("text size is %d", text_size);
+  buff[buff_counter] = '\0';
+  printf("%s\n",buff);
+  if (strcmp("fundarwinvariintprinti",buff) == 0) {
+    printf("true");
+  } else {
+    printf("flase");
+  }
   return tokens;
 }
 
 char *peak(int i) {
   char *text;
   if (texts_count + 1 > text_size) {
-    printf("reached here");
     return NULL;
   }
   return &global_texts[texts_count + i];
 }
 
-char *consume(int i) {
+char *consume() {
   char *text;
   if (texts_count + 1 > text_size) {
-    printf("reached here");
     return NULL;
   }
-  char *temp = &global_texts[texts_count + i];
+  char *temp = &global_texts[texts_count];
   texts_count++;
   return temp;
 }
